5336100264971fa3e7fc76734df84d34dc8e6a82
Surma <surma@surmair.de>
2019-07-16 11:12:37 +0100

    Add server to watch task (#110)
    * Add server to watch task
    
    * Adding no-cache

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,14 @@ import htmlEntryPlugin from './lib/html-entry-plugin.js';
 import postCSSBuild from './lib/postcss-build.js';
 import eleventyBuild from './lib/11ty-build.js';
 import globInputPlugin from './lib/glob-input-plugin';
+import httpServer from './lib/http-server';
 
 export default async function({ watch }) {
   await Promise.all([postCSSBuild('src/**/*.css', '.build-tmp', { watch })]);
   await eleventyBuild({ watch });
+  if (watch) {
+    httpServer();
+  }
 
   return {
     output: {
