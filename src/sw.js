e565bc6970c34a98c8a4f99f2c57def3e530f363
Surma <surma@surma.link>
2019-07-18 17:42:00 +0100

    Reload all clients after nuke

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,16 @@
-self.registration.unregister();
+function run() {
+  // Is this actually being executed in a ServiceWorker?
+  if (!self.clients && !self.registration) {
+    return;
+  }
+  // Nuke the service worker.
+  self.registration.unregister();
+
+  // Reload all open pages.
+  self.clients.matchAll().then(clients => {
+    clients
+      .filter(client => client.type === 'window')
+      .forEach(client => client.navigate(client.url));
+  });
+}
+run();
