1547fb62f47d609ea1141e6e115c17823803f580
Surma <surma@surmair.de>
2019-07-22 12:04:00 +0100

    Add support for CSS imports (#124)

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ const mkdirp = require('mkdirp');
 module.exports = {
   plugins: [
     require('postcss-nested'),
+    require('postcss-import'),
     require('postcss-modules')({
       getJSON: function(cssFileName, json, outputFileName) {
         const parsedPath = path.parse(outputFileName);
