83adf531b914d5c2a28bf8e0184645a30e8e61be
Jake Archibald <jaffathecake@gmail.com>
2019-07-29 17:59:06 +0100

    Sessions promo (#168)
    * Sessions promo
    
    * Better audience pic
    
    * Responsive images

--- src/_includes/previously/script.js
+++ src/_includes/previously/script.js
@@ -1,12 +1,15 @@
 // Image lazy loading
-const observer = new IntersectionObserver(entries => {
-  for (const { isIntersecting, target } of entries) {
-    if (isIntersecting === false) continue;
-    target.src = target.dataset.src;
-    target.style.opacity = 1;
-    observer.unobserve(target);
-  }
-});
+const observer = new IntersectionObserver(
+  entries => {
+    for (const { isIntersecting, target } of entries) {
+      if (!isIntersecting) continue;
+      target.src = target.dataset.src;
+      target.style.opacity = 1;
+      observer.unobserve(target);
+    }
+  },
+  { margin: '10%' },
+);
 
 const imgs = document.currentScript.parentNode.querySelectorAll(
   'img[data-src]',
