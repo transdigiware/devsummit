40bd0499129973e4992320b129d5dd130d0742ba
Surma <surma@surma.link>
2019-07-30 16:14:13 +0100

    Review

--- src/_data/conf.js
+++ src/_data/conf.js
@@ -1,7 +1,1 @@
-const {
-  start,
-  end,
-  utcOffset,
-  conferenceName,
-} = require('../../lib/confbox-config');
-module.exports = { start, end, utcOffset, conferenceName };
+module.exports = require('../../lib/confbox-config');
