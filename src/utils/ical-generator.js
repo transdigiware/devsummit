647aba4559a0a34fd778f8f9c71ce7560792dd85
Surma <surma@surma.link>
2019-07-31 15:21:45 +0100

    Make ics file valid

--- src/utils/ical-generator.js
+++ src/utils/ical-generator.js
@@ -8,15 +8,21 @@ function timestampToCalDate(timestamp) {
 }
 
 module.exports = function({ name, start, end }) {
+  const dtstart = timestampToCalDate(start);
+  const dtend = timestampToCalDate(end);
   return unindent(`
     BEGIN:VCALENDAR
     VERSION:2.0
     PRODID:-//hacksw/handcal//NONSGML v1.0//EN
     BEGIN:VEVENT
-    DTSTART:${timestampToCalDate(start)}
-    DTEND:${timestampToCalDate(end)}
+    DTSTAMP:${timestampToCalDate(0)}
+    DTSTART:${dtstart}
+    DTEND:${dtend}
     SUMMARY:${name}
+    UID:${dtstart}-${dtend}@google.com
     END:VEVENT
     END:VCALENDAR
-  `);
+  `)
+    .split('\n')
+    .join('\r\n');
 };
