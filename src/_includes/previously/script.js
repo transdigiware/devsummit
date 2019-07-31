e81b118e0e2b6b6687b3c2bb471532c6b5cb568f
Surma <surma@surmair.de>
2019-07-31 16:22:41 +0100

    Cross browser (#200)
    * PE for IO
    
    * A desparate attempt
    
    * Lol opacity
    
    * Adjustments for iOS grid implementation

--- src/_includes/previously/script.js
+++ src/_includes/previously/script.js
@@ -1,17 +1,28 @@
-// Image lazy loading
-const observer = new IntersectionObserver(
-  entries => {
-    for (const { isIntersecting, target } of entries) {
-      if (!isIntersecting) continue;
-      target.src = target.dataset.src;
-      target.style.opacity = 1;
-      observer.unobserve(target);
-    }
-  },
-  { margin: '10%' },
-);
+(function() {
+  const imgs = document.currentScript.parentNode.querySelectorAll(
+    'img[data-src]',
+  );
 
-const imgs = document.currentScript.parentNode.querySelectorAll(
-  'img[data-src]',
-);
-for (const img of imgs) observer.observe(img);
+  if (!self.IntersectionObserver) {
+    imgs.forEach(img => {
+      img.src = img.dataset.src;
+      img.style.opacity = 1;
+    });
+    return;
+  }
+
+  // Image lazy loading
+  const observer = new IntersectionObserver(
+    entries => {
+      for (const { isIntersecting, target } of entries) {
+        if (!isIntersecting) continue;
+        target.src = target.dataset.src;
+        target.style.opacity = 1;
+        observer.unobserve(target);
+      }
+    },
+    { margin: '10%' },
+  );
+
+  for (const img of imgs) observer.observe(img);
+})();
